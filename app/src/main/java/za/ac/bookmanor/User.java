b18f3553c951f5c372b47c3035e5680d4ec33d04
Roxanne-oss <roxy.smith2002@gmail.com>
2022-05-06 14:45:48 +0200

    User class added

---
+++ app/src/main/java/za/ac/bookmanor/User.java
@@ -0,0 +1,36 @@
+package za.ac.bookmanor;
+public class User
+{
+    //Declarations
+    private String fullName;
+    private String email;
+    private String hashedPassword;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public static void setFullName(String fullName) {
+        fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public static void setEmail(String email) {
+        email = email;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public static void setHashedPassword(String hashedPassword) {
+        hashedPassword = hashedPassword;
+    }
+
+
+
+
+}
