8e35e5d1ba302ad11615f9ba3ee67655bfc8b81a
Roxanne-oss <roxy.smith2002@gmail.com>
2022-05-11 10:55:29 +0200

    bug fix

--- app/src/main/java/za/ac/bookmanor/CreateNewAccount.java
+++ app/src/main/java/za/ac/bookmanor/CreateNewAccount.java
@@ -6,10 +6,8 @@ import android.os.Bundle;
 
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -22,8 +20,6 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class CreateNewAccount extends AppCompatActivity {
 
